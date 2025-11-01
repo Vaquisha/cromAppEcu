fc37e0a98a130991f69df6c5b947c8cb10dccb81
Vaquisha <carlosjaviervaca@hotmail.es>
2025-11-01 14:42:44 -0500

    Adición de rutas dinámicas

--- app/_layout.js
+++ app/_layout.js
@@ -1,13 +1,13 @@
 import { Stack } from "expo-router";
 import { fonts } from "../fonts/fonts";
 
-export default function StackLayout() {
+export default function RootLayout() {
     return(
     <Stack screenOptions={{
          headerTitleStyle: { fontFamily: fonts.MontserratBold }
     }}> 
         <Stack.Screen name="index" options={{ headerShown: false }}/>
-        <Stack.Screen name="edit" options={{title: "Editar"}}/>
+        <Stack.Screen name="exercises/[id]" options={{title: "Editar"}}/>
         <Stack.Screen name="add" options={{title: "Añadir"}}/>
     </Stack>
     )
